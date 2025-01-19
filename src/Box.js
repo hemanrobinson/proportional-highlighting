b5a189a882a850332fc17c744a09b25a529a00a6
Heman Robinson <heman.robinson@gmail.com>
2025-01-19 13:50:43 -0500

    box plot

--- src/Box.js
+++ src/Box.js
@@ -33,58 +33,59 @@ Box.draw = ( selection, label, x, y, width, height, values, valuesSelected ) => 
             .range([ height * ( 1 - margin ), height * margin ]);
     Graph.draw( selection, label, x, y, width, height, yScale, false );
     
-    // create dummy data
-    var data = [12,19,11,13,12,22,13,4,15,16,18,19,20,12,11,9]
+    // Get the data.
+    let data = [];
+    values.forEach(( value ) => { data.push( value[ 1 ]); });
 
-    // Compute summary statistics used for the box:
-    var data_sorted = data.sort(d3.ascending)
-    var q1 = d3.quantile(data_sorted, .25)
-    var median = d3.quantile(data_sorted, .5)
-    var q3 = d3.quantile(data_sorted, .75)
-    var interQuantileRange = q3 - q1
-    var min = q1 - 1.5 * interQuantileRange
-    var max = q1 + 1.5 * interQuantileRange
-
-    // Show the Y scale
-    var y = d3.scaleLinear()
-      .domain([0,24])
-      .range([height, 0]);
-
+    // Compute summary statistics.
+    const data_sorted = data.sort( d3.ascending ),
+         q1 = d3.quantile( data_sorted, .25 ),
+         median = d3.quantile( data_sorted, .5 ),
+         q3 = d3.quantile( data_sorted, .75 ),
+         interQuantileRange = q3 - q1,
+         min = Math.max( q1 - 1.5 * interQuantileRange, data_sorted[ 0 ]),
+         max = Math.min( q1 + 1.5 * interQuantileRange, data_sorted[ data_sorted.length - 1 ]);
+            
     // Draw the box plot.
-    var center = height / 2;
-    selection.selectAll( ".all" )
-        .append("line") // main vertical line
-            .attr("x1", center)
-            .attr("x2", center)
-            .attr("y1", y(min) )
-            .attr("y2", y(max) )
-            .classed( 'all', true )
-        .append("rect") // box
-            .attr("x", center - width/2)
-            .attr("y", y(q3) )
-            .attr("height", (y(q1)-y(q3)) )
-            .attr("width", width )
-            .classed( 'all', true )
-        .data([min, median, max])
-        .enter()
-        .append("line") // median, min and max horizontal lines
-            .attr("x1", center-width/2)
-            .attr("x2", center+width/2)
-            .attr("y1", function(d){ return(y(d))} )
-            .attr("y2", function(d){ return(y(d))} )
+    const center = width / 2,
+        boxWidth = width / 4;
+    selection
+        .append( "line" )   // whiskers
+            .attr( "x1", center )
+            .attr( "x2", center )
+            .attr( "y1", yScale( min ))
+            .attr( "y2", yScale( max ))
             .classed( 'all', true );
-  
-    // Draw the box plot.
-//    selection.selectAll( ".all" )
-//        .data( values )
-//        .enter()
-//        .append( "rect" )
-//        .classed( 'all', true )
-//        .attr( "x", ( d ) => xScale( d[ 0 ]))
-//        .attr( "y", ( d ) => ( d[ 1 ] >= 0 ) ? yScale( d[ 1 ]) : yScale( 0 ))
-//        .attr( "width", xScale.bandwidth())
-//        .attr( "height", ( d ) => (( xScale.domain().indexOf( d[ 0 ]) >= 0 ) ? Math.max( 0,
-//             ( d[ 1 ] >= 0 ) ? ( yScale( 0 ) - yScale( d[ 1 ])) : ( yScale( d[ 1 ]) - yScale( 0 ))) : 0 ));
+    selection
+        .append( "rect" )   // box
+            .attr( "x", center - boxWidth / 2 )
+            .attr( "y", yScale( q3 ))
+            .attr( "width", boxWidth )
+            .attr( "height", ( yScale( q1 ) - yScale( q3 )))
+            .classed( 'all', true );
+    selection
+        .append( "line" )   // min
+            .attr( "x1", center - boxWidth / 2 )
+            .attr( "x2", center + boxWidth / 2 )
+            .attr( "y1", yScale( min ))
+            .attr( "y2", yScale( min ))
+            .classed( 'all', true );
+    selection
+        .append( "line" )   // median
+            .attr( "x1", center - boxWidth / 2 )
+            .attr( "x2", center + boxWidth / 2 )
+            .attr( "y1", yScale( median ))
+            .attr( "y2", yScale( median ))
+            .classed( 'all', true );
+    selection
+        .append( "line" )   // max
+            .attr( "x1", center - boxWidth / 2 )
+            .attr( "x2", center + boxWidth / 2 )
+            .attr( "y1", yScale( max ))
+            .attr( "y2", yScale( max ))
+            .classed( 'all', true );
+
+
 //    selection.selectAll( ".selected" )
 //        .data( valuesSelected )
 //        .enter()
